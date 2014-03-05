bc2ec06be93d31e8c84753cef557afcb5a6e0120
Cian Wilson <cwilson@ldeo.columbia.edu>
2014-03-05 14:36:33 -0500

    Fix build problems in Simpson cases.  Also speed up the generation by not running the initial condition code, instead importing from a new python module.

---
+++ benchmarks/simpson/3d/c5n3m0_32h/parameters.py
@@ -0,0 +1,9 @@
+c = 5.                                 #Wave speed
+n = 3                                  # permeability exponent
+m = 0                                 # bulk-viscosity exponent
+d = 3                                # dimension
+N = 400                              # number of collocation points
+
+# size of the domain in compaction lengths
+h_on_delta = 32.
+r_max = 25.
