7fa377bdc9c1d2f8f02d1169e76bcb1e445e2eb5
Marc Spiegelman <mspieg@ldeo.columbia.edu>
2015-02-20 12:46:43 -0500

    deleted old simpson benchmarks -- which weren't really tests

--- benchmarks/simpson/2d/c4n2m1/parameters.py
+++
@@ -1,9 +0,0 @@
-c = 4.                                 #Wave speed
-n = 2                                  # permeability exponent
-m = 1                                 # bulk-viscosity exponent
-d = 2                                # dimension
-N = 400                              # number of collocation points
-
-# size of the domain in compaction lengths
-h_on_delta = 64.
-r_max = 25.
--- benchmarks/simpson/3d/c5n3m0/parameters.py
+++
@@ -1,9 +0,0 @@
-c = 5.                                 #Wave speed
-n = 3                                  # permeability exponent
-m = 0                                 # bulk-viscosity exponent
-d = 3                                # dimension
-N = 400                              # number of collocation points
-
-# size of the domain in compaction lengths
-h_on_delta = 64.
-r_max = 25.
