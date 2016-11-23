7512c9a483465be7b7e726f9d1367b3435851ed5
Cian Wilson <cwilson@ldeo.columbia.edu>
2016-11-23 15:01:33 -0500

    Remove build.cmake script.  It doesn't work as expected.

--- descriptions/build.cmake
+++
@@ -1,6 +0,0 @@
-execute_process(
-     COMMAND pdflatex tfbenchmarks.tex && pdflatex tfbenchmarks.tex && bibtex tfbenchmarks.aux && bibtex tfbenchmarks.aux && pdflatex
-tfbenchmarks.tex && pdflatex tfbenchmarks.tex
-     )
-
-
